66d3709955312e7c4d124f913f68331be795af09
Emily Corso <ekcorso@gmail.com>
2021-07-15 11:23:36 -0700

    Initial Commit

---
+++ DrinkGenerator/DrinkGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkGeneratorApp.swift
+//  DrinkGenerator
+//
+//  Created by Emily Corso on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct DrinkGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
