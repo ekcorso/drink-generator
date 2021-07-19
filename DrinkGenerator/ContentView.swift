23ebcfe75a820f2958271a385ca29b10d1dc14f3
Emily Corso <ekcorso@gmail.com>
2021-07-19 12:08:29 -0700

    Draft initial bottle list view.

--- DrinkGenerator/ContentView.swift
+++ DrinkGenerator/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var bottle = ""
+    var bottles = ["gin", "rum", "amaro", "lime juice", "demerera syrup"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("First, what cocktail ingredients do you have on hand?")) {
+                    TextField("Enter your bottles here", text: $bottle)
+                }
+                Section(header: Text("Your bar so far...")) {
+                    List {
+                        ForEach(bottles, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                    .toolbar {
+                        EditButton()
+                    }
+                }
+                Section {
+                    HStack {
+                        Text("Next we'll need to categorize these options.")
+                            .foregroundColor(Color.white)
+                            
+                    Button("Let's go") {
+                        
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 20.0))
+                    }
+                }
+            }
+            .navigationTitle("Cocktail Generator")
+        }
     }
 }
 
