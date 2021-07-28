f7de00a63e5b8799e27cc185e348f79de606da00
Emily Corso <ekcorso@gmail.com>
2021-07-27 17:50:10 -0800

    Update Cocktail API to perform fetching/ parsing

--- DrinkGenerator/CocktailAPI.swift
+++ DrinkGenerator/CocktailAPI.swift
@@ -7,3 +7,23 @@
 
 import Foundation
 
+class CocktailAPI {
+    func parseIngredients(from data: Data) {
+        let decoder = JSONDecoder()
+        
+        if let ingredientArray = try? decoder.decode(IngredientList.self, from: data) {
+            for item in ingredientArray.drinks {
+                // create an array of item.stringIngredient1 or skip this for loop and do something with the whole array this returns
+            }
+        }
+    }
+    
+    func fetchIngredients() {
+        let urlString = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+        
+        if let url = URL(string: urlString), let data = try? Data(contentsOf: url) {
+            parseIngredients(from: data)
+            return
+        }
+    }
+}
