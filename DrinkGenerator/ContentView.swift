e710b3c9954db817d11bea342a07a0b6a9c7d15f
Emily Corso <ekcorso@gmail.com>
2021-07-29 10:53:30 -0700

    Implement cocktail API fetch and display list of bottles
    Implement the cocktail API using a self-executing closure for the
    bottles list. Update the ContentView to show a list of the items
    retrieved.

--- DrinkGenerator/ContentView.swift
+++ DrinkGenerator/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    let bottles: IngredientList = {
+    let bottles: [String] = {
         let urlString = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
         let cocktailAPI = CocktailAPI()
-        let ingredientList = cocktailAPI.decode(IngredientList.self, from: urlString)
+        let drinks = cocktailAPI.decode(IngredientList.self, from: urlString).drinks
         
-        return ingredientList
+        var bottles = [String]()
+        for drink in drinks {
+            let strBottle = drink.strIngredient1
+            bottles.append(strBottle)
+        }
+        
+        return bottles
     }()
     
     @State private var bottle = ""
@@ -24,23 +30,20 @@ struct ContentView: View {
             Form {
                 Section(header: Text("First select the cocktail ingredients you have on hand")) {
                     List {
-//                        ForEach(bottles, id: \.self) {
-//                            Text("\($0.name)")
-//                            //Add subtitle with category?
-//                        }
+                        ForEach(bottles.sorted(), id: \.self) {
+                            Text("\($0.capitalized)")
+                            //Add subtitle with category?
+                        }
+                        HStack {
+                            Spacer()
+                            Button("Add Ingredient") {
+                                bottlesSelected.append(bottle)
+                                bottle = ""
+                            }
+                            Spacer()
+                        }
                     }
                 }
-//                Section(header: Text("First, what cocktail ingredients do you have on hand?")) {
-//                    TextField("Enter your bottles here", text: $bottle)
-//                    HStack {
-//                        Spacer()
-//                        Button("Add Ingredient") {
-//                            bottlesSelected.append(bottle)
-//                            bottle = ""
-//                        }
-//                        Spacer()
-//                    }
-//                }
                 Section(header: Text("Your bar so far...")) {
                     List {
                         ForEach(bottlesSelected, id: \.self) {
