138e1de6eda5ba481999d5f46079c0917aaab27e
Emily Corso <ekcorso@gmail.com>
2021-08-06 16:15:35 -0700

    Draft initial UI
    Draft components for the initial UI page.

--- DrinkGenerator/ContentView.swift
+++ DrinkGenerator/ContentView.swift
@@ -11,16 +11,36 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Choose bottles")) {
+                Text("First, please select up to 10 bottles that you'd like to find cocktails with")
+                
+                Section(header: Text("Add bottles from your home bar")) {
                     NavigationLink(destination: IngredientView()) {
                         Text("Select bottles")
                     }
                 }
-                Section(header: Text("Ready to see some cocktail ideas?")) {
-                    
+                
+                Section(header: Text("Ready to see some cocktail options?")) {
+                    Button("Show me Mr. Potatohead-style recipe ideas") {
+                        showCocktailCombos()
+                        //Use navigation link here instead
+                    }
+                    Button("Show me tried and true recipes") {
+                        showCocktailDbRecipes()
+                        //Use navigation link here instead
+                    }
                 }
+                
             }
+            .navigationTitle("Cocktail Generator")
         }
+    }
+    
+    func showCocktailCombos() {
+        //Take user to generated combos page
+        //Pass values from home bar to that page
+    }
+    func showCocktailDbRecipes() {
+        //Take user to a list of recipes from the cocktail db containing any of the selected ingredients
     }
 }
 
