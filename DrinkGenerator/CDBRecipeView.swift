e25c3e95d4d665058b19d10dc7ebd820dcc94c57
Emily Corso <ekcorso@gmail.com>
2021-08-07 17:35:15 -0700

    Draft model for API cocktail details
    Extends the IngredientList to include additional fields needed to show
    cocktail recipe. Also starts a draft for a view in which to display
    those details.

---
+++ DrinkGenerator/CDBRecipeView.swift
@@ -0,0 +1,23 @@
+//
+//  CDBRecipeView.swift
+//  DrinkGenerator
+//
+//  Created by Emily Corso on 8/6/21.
+//
+
+import SwiftUI
+
+struct CDBRecipeView: View {
+    
+    var body: some View {
+        List {
+            
+        }
+    }
+}
+
+struct CDBRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        CDBRecipeView()
+    }
+}
