1c0bab7f960edbb331a1cf2b62c20fce001dc208
Emily Corso <ekcorso@gmail.com>
2021-08-12 10:03:25 -0700

    Draft ImageView and ImageLoader. Reorganize
    Move helpers into their own class. Add ImageLoader helper. Extract image
    to an ImageView class.

---
+++ DrinkGenerator/Views/ImageView.swift
@@ -0,0 +1,20 @@
+//
+//  ImageView.swift
+//  DrinkGenerator
+//
+//  Created by Emily Corso on 8/11/21.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageView()
+    }
+}
