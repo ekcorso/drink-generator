2ace73780dc58ea9cbcabbb59abd4ab318decada
Emily Corso <ekcorso@gmail.com>
2021-08-12 19:43:34 -0700

    Update Recipe data model. Add detailedRecipe fetch
    Update Recipe data model to comment out the optional strings for now,
    and to update the id value etc. Add detailedRecipe variable calculation
    to RecipeDetailView and update the UI accordingly.

--- DrinkGenerator/Data Model/Recipe.swift
+++ DrinkGenerator/Data Model/Recipe.swift
@@ -13,44 +13,56 @@ struct RecipeList: Codable {
 
 struct Recipe: Codable {
     let strDrink: String
-    let idDrink: Int
+    let idDrink: String
     
     let strGlass: String
     
     //Should the drink thumbnail be a URL or an image? it would need to fetch the image from the url in order to load it
-    let strDrinkThumb: URL
+    let strDrinkThumb: String
     
     let strInstructions: String
     
-    let strIngredient1: String
-    let strIngredient2: String
-    let strIngredient3: String
-    let strIngredient4: String
-    let strIngredient5: String
-    let strIngredient6: String
-    let strIngredient7: String
-    let strIngredient8: String
-    let strIngredient9: String
-    let strIngredient10: String
-    let strIngredient11: String
-    let strIngredient12: String
-    let strIngredient13: String
-    let strIngredient14: String
-    let strIngredient15: String
+    //Ingredients and Measures can be null, so they are optionals
     
-    let strMeasure1: String
-    let strMeasure2: String
-    let strMeasure3: String
-    let strMeasure4: String
-    let strMeasure5: String
-    let strMeasure6: String
-    let strMeasure7: String
-    let strMeasure8: String
-    let strMeasure9: String
-    let strMeasure10: String
-    let strMeasure11: String
-    let strMeasure12: String
-    let strMeasure13: String
-    let strMeasure14: String
-    let strMeasure15: String
+//    let strIngredient1: String
+//    let strIngredient2: String
+//    let strIngredient3: String
+//    let strIngredient4: String
+//    let strIngredient5: String
+//    let strIngredient6: String
+//    let strIngredient7: String
+//    let strIngredient8: String
+//    let strIngredient9: String
+//    let strIngredient10: String
+//    let strIngredient11: String
+//    let strIngredient12: String
+//    let strIngredient13: String
+//    let strIngredient14: String
+//    let strIngredient15: String
+//    
+//    let strMeasure1: String
+//    let strMeasure2: String
+//    let strMeasure3: String
+//    let strMeasure4: String
+//    let strMeasure5: String
+//    let strMeasure6: String
+//    let strMeasure7: String
+//    let strMeasure8: String
+//    let strMeasure9: String
+//    let strMeasure10: String
+//    let strMeasure11: String
+//    let strMeasure12: String
+//    let strMeasure13: String
+//    let strMeasure14: String
+//    let strMeasure15: String
+}
+
+extension Recipe: Identifiable {
+    var id: Int { return Int(idDrink)! }
+}
+
+extension Recipe: Comparable {
+    static func < (lhs: Recipe, rhs: Recipe) -> Bool {
+        lhs.strDrink < rhs.strDrink
+    }
 }
--- DrinkGenerator/Views/RecipeDetailView.swift
+++ DrinkGenerator/Views/RecipeDetailView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     let recipe: DrinkStub
-    
+
+    let detailedRecipe: Recipe = {
+        let urlStringBase = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
+        //recipeIdString is hardcoded for the Tuxedo Cocktail, but should be based on the id of the recipe being passed in
+        let recipeIdString = String(12420)
+        let urlString = urlStringBase + recipeIdString
+        let cocktailAPI = CocktailAPI()
+        let recipes: [Recipe] = cocktailAPI.decode(RecipeList.self, from: urlString).drinks
+        
+        return recipes[0]
+    }()
+        
     var body: some View {
         VStack {
-            ImageView(withURL: recipe.strDrinkThumb)
-            Text(recipe.strDrink)
+            ImageView(withURL: detailedRecipe.strDrinkThumb)
+            Text(detailedRecipe.strDrink)
             Text("""
 Drink recipe instructions go here.
 They could be several lines.
