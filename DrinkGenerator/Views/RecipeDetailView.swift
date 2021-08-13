2e37d6336db5388766be18df04bd5a6d27d5aabe
Emily Corso <ekcorso@gmail.com>
2021-08-13 13:22:47 -0700

    Replace hardcoded recipe ID in RecipeDetailView
    Changes the detailedRecipe property in DetailRecipeView from a
    self-executing closure to a computed property. From there, the id of the
    recipe passed in the previous view can be used for the api call in
    detailedRecipe.

--- DrinkGenerator/Views/RecipeDetailView.swift
+++ DrinkGenerator/Views/RecipeDetailView.swift
@@ -9,31 +9,22 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     let recipe: DrinkStub
-
-    let detailedRecipe: Recipe = {
+    
+    var detailedRecipe: Recipe {
         let urlStringBase = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
-        //recipeIdString is hardcoded for the Tuxedo Cocktail, but should be based on the id of the recipe being passed in
-        let recipeIdString = String(12420)
-        let urlString = urlStringBase + recipeIdString
+        let recipeIdString = String(recipe.id)
+        let urlString: String = urlStringBase + recipeIdString
         let cocktailAPI = CocktailAPI()
         let recipes: [Recipe] = cocktailAPI.decode(RecipeList.self, from: urlString).drinks
         
         return recipes[0]
-    }()
+    }
         
     var body: some View {
         VStack {
             ImageView(withURL: detailedRecipe.strDrinkThumb)
             Text(detailedRecipe.strDrink)
-            Text("""
-Drink recipe instructions go here.
-They could be several lines.
-Like, lots of lines.
-So many ingredients...
-Hell, this could be a recipe for a
-Fino Countdown.
-Let's make sure there's room for some tasty drinks.
-""")
+            Text(detailedRecipe.strInstructions)
         }
     }
 }
