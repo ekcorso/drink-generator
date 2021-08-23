94e1f25d9ee6cfb5a5e6036fbfb3776c7c99a642
Emily Corso <ekcorso@gmail.com>
2021-08-23 13:34:29 -0700

    Update ComboView UI to test ComboGenerator

--- DrinkGenerator/Views/ComboView.swift
+++ DrinkGenerator/Views/ComboView.swift
@@ -8,22 +8,32 @@
 import SwiftUI
 
 struct ComboView: View {
-    @EnvironmentObject var ingredients: [Bottle]
+    var ingredients: [Bottle]
     
     //takenBy is temporarily hardcoded. Still deciding if this should be decided by the user or not
     var takenBy: Int = 3
     
     var allCombos: [[Bottle]] { ComboGenerator().generateAllIngredientCombos(ingredients: ingredients, takenBy: takenBy)
     }
-    
+        
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        ForEach(Array(zip(allCombos.indicies, allCombos
+//        )), id: \.0) { index, combo in
+//            Text("\(combo.description)")
+//        }
+        Button("Print Combos") {
+            for combo in allCombos.enumerated() {
+                print(combo.element)
+//                for item in combo {
+//                    print(item.name)
+//                }
+            }
+        }
     }
 }
 
 struct ComboView_Previews: PreviewProvider {
     static var previews: some View {
-        ComboView()
-            //.environmentObject([Bottle.example])
+        ComboView(ingredients: [Bottle.example1, Bottle.example2, Bottle.example3])
     }
 }
