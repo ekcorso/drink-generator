75b8d6d4c9460d2b9a300d4e080e7f7f0345088f
Emily Corso <ekcorso@gmail.com>
2021-08-23 14:15:05 -0700

    Update ComboView UI, get ComboGenerator working
    Updates the ComboView UI to show the ingredients for one random combo.

--- DrinkGenerator/Views/ComboView.swift
+++ DrinkGenerator/Views/ComboView.swift
@@ -13,27 +13,40 @@ struct ComboView: View {
     //takenBy is temporarily hardcoded. Still deciding if this should be decided by the user or not
     var takenBy: Int = 3
     
-    var allCombos: [[Bottle]] { ComboGenerator().generateAllIngredientCombos(ingredients: ingredients, takenBy: takenBy)
+    var randomCombo: [Bottle] { ComboGenerator().generateAllIngredientCombos(ingredients: ingredients, takenBy: takenBy).randomElement()!
     }
         
     var body: some View {
+        VStack {
+            Section {
+                Text("Try this out:")
+                    .bold()
+                    .font(.title2)
+            }
+            
+            Section {
+                ForEach(randomCombo) { item in
+                    Text(item.name)
+                }
+            }
+        }
 //        ForEach(Array(zip(allCombos.indicies, allCombos
 //        )), id: \.0) { index, combo in
 //            Text("\(combo.description)")
 //        }
-        Button("Print Combos") {
-            for combo in allCombos.enumerated() {
-                print(combo.element)
-//                for item in combo {
-//                    print(item.name)
-//                }
-            }
-        }
+//        Button("Print Combos") {
+//            for combo in allCombos.enumerated() {
+//                print(combo.element)
+////                for item in combo {
+////                    print(item.name)
+////                }
+//            }
+//        }
     }
 }
 
 struct ComboView_Previews: PreviewProvider {
     static var previews: some View {
-        ComboView(ingredients: [Bottle.example1, Bottle.example2, Bottle.example3])
+        ComboView(ingredients: [Bottle.example1, Bottle.example2, Bottle.example3, Bottle.example4])
     }
 }
