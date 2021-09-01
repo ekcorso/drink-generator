5350d3f9072b8bd52480b0606d6c88ce9b41719e
Emily Corso <ekcorso@gmail.com>
2021-09-01 12:26:02 -0700

    Update Bottle example strings

--- DrinkGenerator/Data Model/Bottle.swift
+++ DrinkGenerator/Data Model/Bottle.swift
@@ -14,10 +14,10 @@ class Bottle: Identifiable, Hashable, ObservableObject {
 
     //var category: String
     
-    static let example1 = Bottle(name: "Smith & Cross", id: UUID())
-    static let example2 = Bottle(name: "Aria Gin", id: UUID())
-    static let example3 = Bottle(name: "Combier", id: UUID())
-    static let example4 = Bottle(name: "Suze", id: UUID())
+    static let example1 = Bottle(name: "Whiskey", id: UUID())
+    static let example2 = Bottle(name: "Vodka", id: UUID())
+    static let example3 = Bottle(name: "Vermouth", id: UUID())
+    static let example4 = Bottle(name: "Tequila", id: UUID())
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(name)
