409ad72ab064b9faa567db10042599d261601863
Emily Corso <ekcorso@gmail.com>
2021-08-31 20:50:44 -0700

    Move urlString creation to CocktailAPI

--- DrinkGenerator/Helpers/CocktailAPI.swift
+++ DrinkGenerator/Helpers/CocktailAPI.swift
@@ -8,6 +8,21 @@
 import Foundation
 
 struct CocktailAPI {
+    var homeBar: HomeBar
+    
+    var urlString: String {
+        // TODO: Guard statement needs to handle the case where bottleList is empty
+        guard let firstBottle = homeBar.bottleList.first?.name else { return "Gin" }
+        let urlBaseString = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i="
+        // TODO: Be sure to convert searchTerm to snakecase before trying to use it
+        let searchTerm = String(firstBottle)
+        return urlBaseString + searchTerm
+    }
+    
+    init(homeBar: HomeBar) {
+        self.homeBar = homeBar
+    }
+    
     func decode<T:Decodable>(_ type: T.Type, from urlString: String) -> T {
         guard let url = URL(string: urlString) else {
             fatalError("Failed to cast urlString as url.")
