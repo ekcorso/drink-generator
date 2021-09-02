e839c656a050e801f831abee3bf9ae32dbf26145
Emily Corso <ekcorso@gmail.com>
2021-09-02 10:42:49 -0700

    Add RequestType enum
    For use in the CocktailAPI class.

---
+++ DrinkGenerator/Data Model/RequestType.swift
@@ -0,0 +1,14 @@
+//
+//  RequestType.swift
+//  DrinkGenerator
+//
+//  Created by Emily Corso on 9/2/21.
+//
+
+import Foundation
+
+enum RequestType {
+    case recipeDetail
+    case ingredientList
+    case drinkStubList
+}
