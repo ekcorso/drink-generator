4e09f1df459d8fb4bb688a92018b9abb815d1f57
Emily Corso <ekcorso@gmail.com>
2021-09-14 12:06:18 -0700

    Add MainView and tab bar items
    Draft MainView so that views can be accessed as tab bar items instead of
    from the ContentView navigation page. ComboView is commented out because
    it needs to be fixed before it can be used.

---
+++ DrinkGenerator/Views/MainView.swift
@@ -0,0 +1,34 @@
+//
+//  MainView.swift
+//  DrinkGenerator
+//
+//  Created by Emily Corso on 9/14/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            IngredientView()
+                .tabItem {
+                    Label("Home Bar", systemImage: "house")
+                }
+            RecipeListView()
+                .tabItem {
+                    Label("Recipes", systemImage: "list.bullet.rectangle")
+                }
+//            ComboView()
+//                .tabItem {
+//                    Label("Combos", systemImage: "bolt")
+//                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+            .environmentObject(HomeBar())
+    }
+}
