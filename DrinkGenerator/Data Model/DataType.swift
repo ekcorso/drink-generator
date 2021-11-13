87cee038a65c4806508a1bf23c292c68bf911a96
Emily Corso <ekcorso@gmail.com>
2021-11-12 16:47:23 -0800

    Add DataType enum

---
+++ DrinkGenerator/Data Model/DataType.swift
@@ -0,0 +1,13 @@
+//
+//  DataType.swift
+//  DrinkGenerator
+//
+//  Created by Emily Corso on 11/12/21.
+//
+
+import Foundation
+
+enum DataType: String {
+    case ingredients = "ingredients.txt"
+    case bottleList = "botteList.txt"
+}
