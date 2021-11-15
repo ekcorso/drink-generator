0757af6c562575fff5cea8bd186ac304beb3df8b
Emily Corso <ekcorso@gmail.com>
2021-11-14 20:45:52 -0800

    Add example recipe

--- DrinkGenerator/Data Model/Recipe.swift
+++ DrinkGenerator/Data Model/Recipe.swift
@@ -54,6 +54,8 @@ struct Recipe: Codable {
     let strMeasure13: String?
     let strMeasure14: String?
     let strMeasure15: String?
+    
+    static let example = Recipe(strDrink: "Margarita", idDrink: "11007", strGlass: "Cocktail glass", strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg", strInstructions: "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.", strIngredient1: "Tequila", strIngredient2: "Triple sec", strIngredient3: "Lime juice", strIngredient4: "Salt", strIngredient5: nil, strIngredient6: nil, strIngredient7: nil, strIngredient8: nil, strIngredient9: nil, strIngredient10: nil, strIngredient11: nil, strIngredient12: nil, strIngredient13: nil, strIngredient14: nil, strIngredient15: nil, strMeasure1: "1 1/2 oz ", strMeasure2: "1/2 oz ", strMeasure3: "1 oz ", strMeasure4: nil, strMeasure5: nil, strMeasure6: nil, strMeasure7: nil, strMeasure8: nil, strMeasure9: nil, strMeasure10: nil, strMeasure11: nil, strMeasure12: nil, strMeasure13: nil, strMeasure14: nil, strMeasure15: nil)
 }
 
 extension Recipe: Identifiable {
